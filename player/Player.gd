81e131e5b2e9f5d0146edd32aafe2699eeab1159
Nathan <nathan@gdquest.com>
2016-12-10 22:25:14 +0100

    Added wall stick and wall jump

--- player/Player.gd
+++ player/Player.gd
@@ -13,14 +13,18 @@ var previous_state = S_IDLE
 var enter_state = false
 var exit_state = false
 
-const S_DEAD = -1
-const S_IDLE = 0
-const S_RUN = 1
-const S_LAND = 4
-const S_JUMP = 2
-const S_FALL = 3
-const S_WALL = 5
-const S_DASH = 6
+var on_floor = false
+var on_wall = false
+
+const S_DEAD = 'dead'
+const S_IDLE = 'idle'
+const S_RUN = 'run'
+const S_LAND = 'land'
+const S_JUMP = 'jump'
+const S_WALL_JUMP = 'walljump'
+const S_FALL = 'fall'
+const S_WALL = 'wall'
+const S_DASH = 'dash'
 
 const S_FLOOR = [S_IDLE, S_RUN]
 const S_AIR = [S_JUMP, S_FALL]
@@ -32,6 +36,8 @@ var direction = 1
 var prev_direction = 1
 var speed = Vector2(0.0,0.0)
 var velocity = Vector2(0.0,0.0)
+var gravity = 0.0
+var wall_jump_direction = 0
 
 var air_timer = 0.0
 const AIR_THRESHOLD = 0.2
@@ -41,10 +47,14 @@ const ACCELERATION_X = 2000
 const ACCELERATION_X_JUMP = 3000
 const FRICTION_FLOOR = 2000
 
+# const JUMP_IMPULSE = 400
+# const GRAVITY = 800
 const JUMP_IMPULSE = 1200
+const JUMP_IMPULSE_WALL = Vector2(400, -800)
 const GRAVITY = 2600
-const FRICTION_WALL = 600
+const GRAVITY_WALL = 600
 const MAX_SPEED_FALL = 800
+const MAX_SPEED_FALL_WALL = 300
 const MAX_SPEED_JUMP = 800
 const STOP_THRESHOLD = 30
 
@@ -53,8 +63,7 @@ const MAX_SPEED_RUN = 800
 
 const MAX_SPEED_WALL_SLIDE = 200
 
-var on_floor = false
-var on_wall = false
+var jump_trigger = false
 
 func _ready():
 	set_fixed_process(true)
@@ -67,6 +76,7 @@ func _ready():
 # INPUT 
 # State management based on input
 func _input(event):
+	jump_trigger = event.is_action_pressed("jump") and not event.is_echo()
 	pass
 
 func _fixed_process(delta):
@@ -88,6 +98,8 @@ func _fixed_process(delta):
 			on_wall = false
 			speed.x /= 2
 			speed.y = 0.0
+		elif previous_state == S_WALL:
+			on_wall = false
 	if enter_state:
 		enter_state = false
 		if state == S_JUMP:
@@ -96,6 +108,16 @@ func _fixed_process(delta):
 		elif state == S_FALL:
 			on_floor = false
 			air_timer = 0.0
+		elif state == S_WALL:
+			on_wall = true
+			speed.x = 0.0
+			speed.y /= 1.8
+		elif state == S_WALL_JUMP:
+			on_wall = false
+			on_floor = false
+			jump_trigger = false
+			speed.x = wall_jump_direction * JUMP_IMPULSE_WALL.x
+			speed.y = JUMP_IMPULSE_WALL.y
 	
 	
 	# MOVEMENT HORIZONTAL ON FLOOR
@@ -134,21 +156,33 @@ func _fixed_process(delta):
 		speed.x *= direction
 	# AIR MOTION
 	elif not on_floor:
-		if previous_state != S_JUMP and state == S_FALL:
+		if not previous_state in [S_JUMP, S_WALL_JUMP] and state == S_FALL:
 			air_timer += delta
 			if air_timer <= AIR_THRESHOLD and jump:
 				go_to_state(S_JUMP)
 				# speed.y = -JUMP_IMPULSE
-		elif state == S_JUMP and speed.y > 0.0:
+		elif state in [S_JUMP, S_WALL_JUMP] and speed.y > 0.0:
 			go_to_state(S_FALL)
 		# X MOVEMENT
 		if moving:
 			speed.x += ACCELERATION_X_JUMP * direction * delta
 	
-	speed.y += GRAVITY * delta
-	speed.y = clamp(speed.y, -MAX_SPEED_JUMP, MAX_SPEED_FALL)
 
-	
+	# WALL MOTION
+	if on_wall:
+		if jump_trigger:
+			go_to_state(S_WALL_JUMP)
+
+
+	# GRAVITY and WALL FRICTION
+	if on_wall and state == S_WALL:
+		gravity = GRAVITY_WALL
+	else: 
+		gravity = GRAVITY
+	speed.y += gravity * delta
+	speed.y = clamp(speed.y, -MAX_SPEED_JUMP, MAX_SPEED_FALL)
+	if speed.y > MAX_SPEED_FALL_WALL and state == S_WALL:
+		speed.y = MAX_SPEED_FALL_WALL
 	# APPLYING MOVEMENT
 	speed.x = clamp(speed.x, -MAX_SPEED, MAX_SPEED)
 	velocity.x = speed.x * delta
@@ -158,14 +192,33 @@ func _fixed_process(delta):
 	# COLLISIONS
 	if is_colliding():
 		var normal = get_collision_normal()
-		var collision_with_floor = normal == Vector2(0, -1)
-
-		if not on_floor and collision_with_floor:
-			go_to_state(S_IDLE)
 		
+		var col_with_floor = normal == Vector2(0, -1)
+		var col_with_wall = normal == Vector2(1, 0) or normal == Vector2(-1, 0)
+		var col_with_ceiling = normal == Vector2(0, 1)
+
+		# print(col_with_wall)
+
+		if col_with_floor and not on_floor:
+			go_to_state(S_IDLE)
+			on_floor = true
+		# print(str(on_floor) + ' / ' + str(on_wall))
+		if not on_floor:
+			if col_with_floor:
+				go_to_state(S_IDLE)
+			elif col_with_wall and state != S_WALL:
+				if state != S_WALL_JUMP:
+					go_to_state(S_WALL)
+					wall_jump_direction = normal.x
+		elif on_floor and state == S_WALL:
+			go_to_state(S_IDLE)
+		# if not col_with_wall and state == S_WALL:
+		# 	go_to_state(S_IDLE)
 		velocity = normal.slide(velocity)
 		velocity = move(velocity)
 	elif state in S_FLOOR:
+		go_to_state(S_FALL)
+	elif state == S_WALL and moving and direction == wall_jump_direction:
 		go_to_state(S_FALL)
 	pass
 
@@ -174,12 +227,14 @@ func _fixed_process(delta):
 
 
 func go_to_state(new_state):
-	if new_state != state:
-		previous_state = state
+	if new_state == state:
+		return false
+	
+	previous_state = state
 	state = new_state
 	enter_state = true
 	exit_state = true
 
 	if DEBUG_STATE:
-		print(NAME + " state: " + str(state) + " // previous: " + str(previous_state))
+		print(NAME + ": " + str(state) + " // " + str(previous_state))
 	pass
