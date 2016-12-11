87077ea479ba7eb90ac9e74f170f2ee8338a55c6
Nathan <nathan@gdquest.com>
2016-12-11 20:24:14 +0100

    Finished danger zone code

--- level/danger_zone.gd
+++ level/danger_zone.gd
@@ -1,15 +1,11 @@
 tool
 extends Node2D
-# 
-# use encloses(), intersects() or has_point() to check overlap with the player
+# Testing out drawing in the editor - could be replaced by Area2d or another collider
+# Use encloses(), intersects() or has_point() to check overlap with the player
 
-var bbox = Rect2(Vector2(100, 100), Vector2(100, 100))
-var position = Vector2(100,100)
-var size = Vector2(200,200)
+var bbox = Rect2()
 var timeout = 0.0
-
-# func _draw():
-#     pass
+var active = false
 
 func _draw():
     var color = Color(0.0, 0.0, 0.0)
@@ -17,32 +13,42 @@ func _draw():
 
 func draw_zone_outline( color ):
 	var points = Vector2Array()
-	points.push_back(position)
-	points.push_back(Vector2(position.x + size.x, position.y))
-	points.push_back(position + size)
-	points.push_back(Vector2(position.x, position.y + size.y))
-	points.push_back(position)
+	
+	points.push_back(Vector2())
+	points.push_back(Vector2(bbox.end.x, 0))
+	points.push_back(bbox.end)
+	points.push_back(Vector2(0, bbox.end.y))
+	points.push_back(Vector2())
 
 	for index in range(4):
 		draw_line(points[index], points[index+1], color)
 
 
-func _ready():
-    init_zone(Vector2(100,100), Vector2(200,200), 3.0)
-    set_process(true)
-    pass
-
-func init_zone(_position = Vector2(), _size = Vector2(), _timeout = 3.0):
-    bbox = Rect2(_position, _size)
+func set_danger_zone(_position = Vector2(), _size = Vector2(), _timeout = 3.0):
+    set_pos(_position)
+    bbox = Rect2(Vector2(), _size)
     timeout = _timeout
+    active = true
+
     if get_tree().is_editor_hint():
         update()
+    
     set_process(true)
     pass
 
+
+func _ready():
+    set_danger_zone(Vector2(285,100), Vector2(300,200), 3.0)
+    set_process(true)
+    pass
+
+
 func _process(delta):
-    if not get_tree().is_editor_hint():
-        timeout -= delta
-        if timeout < 0:
-            queue_free()
+    if get_tree().is_editor_hint():
+        update()
+    else:
+        if active:
+            timeout -= delta
+            if timeout < 0:
+                active = false
     pass
