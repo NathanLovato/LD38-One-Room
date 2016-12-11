24c4667ca82d63c2173a223e67266761e0afe653
Nathan <nathan@gdquest.com>
2016-12-11 20:45:12 +0100

    Simplified danger_zone code

--- level/danger_zone.gd
+++ level/danger_zone.gd
@@ -1,11 +1,10 @@
 tool
 extends Node2D
+
 # Testing out drawing in the editor - could be replaced by Area2d or another collider
 # Use encloses(), intersects() or has_point() to check overlap with the player
-
 var bbox = Rect2()
 var timeout = 0.0
-var active = false
 
 func _draw():
     var color = Color(0.0, 0.0, 0.0)
@@ -13,7 +12,7 @@ func _draw():
 
 func draw_zone_outline( color ):
 	var points = Vector2Array()
-	
+
 	points.push_back(Vector2())
 	points.push_back(Vector2(bbox.end.x, 0))
 	points.push_back(bbox.end)
@@ -24,31 +23,20 @@ func draw_zone_outline( color ):
 		draw_line(points[index], points[index+1], color)
 
 
-func set_danger_zone(_position = Vector2(), _size = Vector2(), _timeout = 3.0):
+func initialize(_position = Vector2(), _size = Vector2()):
     set_pos(_position)
     bbox = Rect2(Vector2(), _size)
-    timeout = _timeout
-    active = true
-
+    
     if get_tree().is_editor_hint():
         update()
-    
-    set_process(true)
-    pass
 
 
-func _ready():
-    set_danger_zone(Vector2(285,100), Vector2(300,200), 3.0)
+func activate(_timeout = 3.0):
+    timeout = _timeout
     set_process(true)
-    pass
 
 
 func _process(delta):
-    if get_tree().is_editor_hint():
-        update()
-    else:
-        if active:
-            timeout -= delta
-            if timeout < 0:
-                active = false
-    pass
+    timeout -= delta
+    if timeout < 0:
+        set_process(false)
--- level/game.gd
+++ level/game.gd
@@ -1,3 +1,4 @@
+tool
 extends Node
 
 # Controls the main state of the game, the score, and player death
@@ -8,11 +9,11 @@ const S_PLAY = "play"
 const S_PAUSE = "pause"
 const S_GAME_OVER = "gameover"
 
+var viewport_size = Vector2()
 
 # Gameplay generation
 var timer_explosion = 0.0
 const TIME_EXPLODE = 3.0
-
 
 var max_danger_zones = 4
 var difficulty = 1
@@ -20,12 +21,12 @@ var difficulty = 1
 # NODES
 onready var player = get_node("Player")
 onready var score = get_node("Score/Base")
-var danger_zone = preload("res://myscene.scn")
+onready var danger_container = get_node("Zones") 
+onready var danger_zone = preload("res://level/Danger.tscn")
 
 func _ready():
+    viewport_size = get_viewport().get_rect().size
     set_process(true)
-
-    pass
 
 func _process(delta):
     if game_state == S_PLAY:
@@ -34,6 +35,14 @@ func _process(delta):
 
 # Generates danger zones in the game
 # Rectangles that appear and after some time, kill the player if he overlaps them
-func create_danger_zone():
-
+func create_danger_zone(position, size, timeout):
+    var zone = danger_zone.instance()
+    zone.set_danger_zone(position, size, TIME_EXPLODE)
+    danger_container.add_child(zone)
     pass
+
+func test_danger_zones():
+    if not get_tree().is_editor_hint():
+        return false
+    
+    create_danger_zone()
